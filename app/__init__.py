39b01128a27007ab3cb7e13fc4f43dda624876bc
Dmitry <voronov-d88@yandex.ru>
2017-12-14 08:46:10 +0000

    Add autherization

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
-from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-from app import models
-
 admin = Admin(app, name='pizza bot', template_mode='bootstrap3')
+from app import models, views
 admin.add_view(ModelView(models.Pizza, db.session))
 admin.add_view(ModelView(models.PizzaSize, db.session))
--- config.py
+++ config.py
@@ -9,3 +9,5 @@ SECRET_KEY = 'super secret key'
 DEBUG=1
 site_ip_address = '0.0.0.0'
 site_port = 8080
+admin_name = os.environ["FLASK_ADMIN"]
+admin_password = os.environ["FLASK_PASSWORD"]
